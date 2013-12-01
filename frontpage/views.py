f66cecda0f4a771b4c6de388fdf105820661f979
sypa <syp@riseup.net>
2013-12-01 06:46:45 +0200

    reverse sorting order on frontpage, newest first.

--- frontpage/views.py
+++ frontpage/views.py
@@ -8,7 +8,7 @@ def frontpage(request):
     # @TODO: Use weight and featured fields, which need to be implemented.
     # @TODO: use_editable for mezzanines inline editing?
     # @TODO: probably we should support other conntent types here?
-    articles = Item.objects.order_by('updated_at')[:5]
+    articles = Item.objects.order_by('-updated_at')[:5]
     articles = paginate(articles, request.GET.get("page", 1),
                         settings.BLOG_POST_PER_PAGE,
                         settings.MAX_PAGING_LINKS)
