935cf69b138fa4747a8a6f854a5a8963b692f599
sypa <syp@riseup.net>
2013-12-29 03:20:17 +0100

    pep8

--- aggregator/models.py
+++ aggregator/models.py
@@ -19,22 +19,25 @@ logger = logging.getLogger(__name__)
 
 fetched_feed_file = Signal()
 
+
 class FeedFile(models.Model):
-    """The class that fetches the feed file 
-    
+    """The class that fetches the feed file
+
     This class fetches the feed file and stores the feed file contents. It also
     takes care to minimize used bandwidth and supports archiving the feed files
     to the local storage.
     """
     #: The url from where the feed file is to be fetched. Has to be unique.
     url = models.URLField(unique=True)
-    #: The contents of the feed file. 
+    #: The contents of the feed file.
     body = models.TextField()
     #: When this feed file has been updated the last time. Defaults to now.
     updated_at = models.DateTimeField(auto_now=True)
-    #: The ETag is a special header that some feed publishers support to reduce bandwidth.
+    #: The ETag is a special header that some feed publishers support to reduce
+    #: bandwidth.
     etag = models.CharField(max_length=128, blank=True)
-    #: The last modified header is also supported by some publishers to reduce bandwidth.
+    #: The last modified header is also supported by some publishers to reduce
+    #: bandwidth.
     modified = models.CharField(max_length=40, blank=True)
 
     def __unicode__(self):
@@ -42,21 +45,23 @@ class FeedFile(models.Model):
 
     def fetch(self):
         """Fetches the feed file in a bandwidth-friendly way
-        
-        Tries to only fetch the new items if the feed publisher supports ETag 
+
+        Tries to only fetch the new items if the feed publisher supports ETag
         and/or last_modified HTTP headers. It also archives the fetched
-        file and parses the file by using the model Feed. 
+        file and parses the file by using the model Feed.
         """
         logger.info("fetching feed at '%s'..." % self.url)
         try:
-            headers = {'user-agent': feedparser.USER_AGENT, 'If-None-Match': self.etag, 'If-Modified-Since': self.modified}
+            headers = {'user-agent': feedparser.USER_AGENT,
+                       'If-None-Match': self.etag,
+                       'If-Modified-Since': self.modified}
             req = requests.get(self.url, headers=headers, verify=False)
         except Exception as e:
             logger.error(e)
             return None
-        
+
         """
-        If the HTTP status code is 304, the feed didn't change since the last 
+        If the HTTP status code is 304, the feed didn't change since the last
         time the file was fetched, thus no further processing useful.
         """
         if req.status_code == 304:
@@ -133,19 +138,20 @@ def parse_feed_file(sender, **kwargs):
 
 class Item(models.Model):
     feed = models.ForeignKey(Feed)
-    
+
     title = models.CharField(max_length=512)
     link = models.URLField()
     tags = TaggableManager()
     description = models.TextField()
-    #: Holds the datetime when this item was published or updated. 
+    #: Holds the datetime when this item was published or updated.
     updated_at = models.DateTimeField()
-    #: If the feed comes with the full content (not only the summary), populate this. 
-    content = models.TextField(blank = True)
+    #: If the feed comes with the full content (not only the summary), populate
+    #: this.
+    content = models.TextField(blank=True)
     #: Stores the way content was obtained:
     content_source = models.CharField(max_length=32)
-    #: This holds the HTML of the item link at the time it got parsed. 
-    link_html = models.TextField(blank = True)
+    #: This holds the HTML of the item link at the time it got parsed.
+    link_html = models.TextField(blank=True)
 
     def __unicode__(self):
         return "[%s] %s" % (self.feed.title, self.title)
@@ -161,7 +167,7 @@ class Item(models.Model):
         else:
             updated_at = parse_time(entry.published_parsed)
         item, new = cls.objects.get_or_create(
-            feed=feed, link=entry.link, title=entry.title, 
+            feed=feed, link=entry.link, title=entry.title,
             defaults={
                 # @TODO: sanitize html (XSS,..)
                 'description': entry.description,
@@ -170,20 +176,22 @@ class Item(models.Model):
 
         if 'tags' in entry and 'term' in entry.tags[0]:
             item.tags.add(*[tag.term for tag in entry.tags])
-        
+
         # try to get the entry content in suitable format if supplied
         # see http://pythonhosted.org/feedparser/reference-entry-content.html
         if 'content' in entry:
             for c in entry.content:
-                if c.type in ['text/plain','text/html','application/xhtml+xml']:
+                if c.type in ['text/plain', 'text/html',
+                              'application/xhtml+xml']:
                     item.content = c.value
                     item.content_source = 'feed'
                     # for now take the first suitable content and exit the loop
                     break
-        
-        # fetching and storing the HTML of the linked web page 
+
+        # fetching and storing the HTML of the linked web page
         # @TODO only if content is empty or always for archiving?
-        req = requests.get(item.link, headers={'user-agent': 'readability'}, verify=False)
+        req = requests.get(item.link, headers={'user-agent': 'readability'},
+                           verify=False)
         if req.status_code != 200:
             raise requests.HTTPError("error while fetching HTML of '%s': %s" %
                                      (item.link, req.status_code))
