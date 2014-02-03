a10537c4cbae01648d32ebad6df4eb04eef3affb
sypa <syp@riseup.net>
2014-02-03 15:39:18 +0200

    initial working backend draft, using backbone.js

--- theme/static/admin.js
+++ theme/static/admin.js
@@ -1,53 +1,114 @@
+// @TODO Set djangos anti-cross-site-request-forgery-token.
+//var token = $('meta[name="csrf-token"]').attr('content');
+//xhr.setRequestHeader('X-CSRFToken', token);
+
 $(document).ready(function() {
     window.bericht = window.bericht || {};
-    window.bericht.admin = admin = {
 
-        init: function() {
-            admin.container = $('#article-list');
-            $.getJSON('/articles.json')
-                .success(admin.render_articles)
-                .error(function(error) {
-                    // @TODO: Handle errors
-                });
-
-            var keys = {
-                'j': admin.select_prev_article,
-                'k': admin.select_next_article
-            };
-
-            $.each(keys, function(k, f) {
-                $(document).bind('keypress.'+k, f);
-            });
-
-        },
-        render_articles: function(data) {
-            $.each(data, admin.render_article);
-
-            admin.articles = $(admin.container.children());
-            admin.select_article(admin.articles.first());
-        },
-        render_article: function(index, item) {
-            $('<article id="' + item.slug + '">' + 
-              '<h2>' + item.title + '</h2>' + 
-              '<div class="description">' + item.description + '</div>' +
-              '</article>')
-                .appendTo(admin.container); 
-        },
-        select_article: function(el) {
-            if (typeof el === 'undefined' || el.length === 0) { return; }
-            if (typeof admin.selected !== 'undefined') {
-                admin.selected.removeClass('selected');
-            }
-            admin.selected = $(el);
-            admin.selected.addClass('selected');
-            // @TODO: scroll to element.
-        },
-        select_prev_article: function() {
-            admin.select_article(admin.selected.prev());
-        },
-        select_next_article: function() {
-            admin.select_article(admin.selected.next());
-        },
-    };
-    admin.init();
+    window.bericht.articles = new ArticleList();
+    new ArticleListView({
+        collection: window.bericht.articles
+    });
 });
+
+var Article = Backbone.Model.extend({
+    defaults: {
+        selected: false,
+    },
+});
+
+var ArticleList = Backbone.Collection.extend({
+    url: '/api/articles',
+    model: Article,
+
+    select: function(article) {
+        if (typeof(this.selected) !== 'undefined') {
+            this.selected.set({selected:false});
+        } else {
+            // @TODO: For the first change - from the default value to a new
+            // one - there's no event triggered. For now: Workaround and
+            // trigger it manually.
+        }
+        article.set({selected:true});
+        this.selected = article;
+    },
+
+    next: function () {
+        var next = this.at(this.indexOf(this.selected) + 1);
+        if (typeof(next) !== 'undefined') this.select(next);
+        return this;
+    },
+
+    prev: function() {
+        var prev = this.at(this.indexOf(this.selected) - 1);
+        if (typeof(prev) !== 'undefined') this.select(prev);
+        return this;
+    },
+
+    parse: function(response) {
+        return response.results;
+    },
+});
+
+var ArticleSidebarView = Backbone.View.extend({
+    tagName: 'li',
+    events: { click: 'select' },
+
+    initialize: function() {
+        this.model.collection.on('change:selected', this.render, this);
+    },
+
+    render: function() {
+        this.$el.attr('id', 'sidebar-' + this.model.get('slug'))
+            .toggleClass('selected', this.model.get('selected'))
+            .html(this.model.get('title'));
+        return this;
+    },
+
+    select: function() {
+        this.model.collection.select(this.model);
+    },
+});
+
+var ArticleView = Backbone.View.extend({
+    tagName: 'article',
+
+    initialize: function() {
+        this.render();
+        this.model.on('change:selected', this.selected, this);
+    },
+
+    selected: function() {
+        $('#content').html(this.$el);
+    },
+    
+    render: function() {
+        this.$el.attr('id', this.model.get('slug'))
+            .append($('<h2>').html(this.model.get('title')))
+            .append($('<div>').html(this.model.get('description')));
+        return this;
+    },
+});
+
+var ArticleListView = Backbone.View.extend({
+    el: "#sidebar-list",
+
+    initialize: function() {
+        this.collection.on('reset', this.addAll, this);
+        this.collection.on('add', this.addOne, this);
+        this.collection.fetch({reset: true});
+    },
+
+    addAll: function() {
+        this.collection.each(this.addOne, this);
+        this.collection.select(this.collection.at(0));
+    },
+
+    addOne: function(article) {
+        new ArticleView({model: article});
+        var sidebar_view = new ArticleSidebarView({model: article});
+        this.$el.append(sidebar_view.render().el);
+    },
+});
+
+
