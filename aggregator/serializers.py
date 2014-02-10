3f475e917610fa990a40fb65d5a5b155f7d40432
sypa <sypa@internet>
2014-02-10 23:39:34 +0200

    add feeds title to the serializer.

--- aggregator/serializers.py
+++ aggregator/serializers.py
@@ -5,7 +5,9 @@ from aggregator.models import Item
 class ArticleSerializer(serializers.ModelSerializer):
     slug = serializers.CharField(source='slug', read_only=True)
     content = serializers.CharField(source='get_content', read_only=True)
+    source = serializers.CharField(source='feed.title', read_only=True)
 
     class Meta:
         model = Item  # @TODO Article, not Item
-        fields = ('id', 'slug', 'title', 'tags', 'content', 'updated_at')
+        fields = ('id', 'slug', 'title', 'tags', 'content', 'updated_at',
+                  'source')
