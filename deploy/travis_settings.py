57e66d6bbf194809e609128123d4ff718c75f18f
sypa <syp@riseup.net>
2014-03-16 09:00:59 +0200

    rename PROJECT_ROOT in travis_settings.py.
    so that it does not overwrite PROJECT_ROOT in the normal settings.py.

--- deploy/travis_settings.py
+++ deploy/travis_settings.py
@@ -3,7 +3,7 @@
 import os
 
 HOME = os.environ['HOME']
-PROJECT_ROOT = HOME + '/bericht-data'
+DATA_ROOT = HOME + '/bericht-data'
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -14,12 +14,12 @@ SECRET_KEY = 'testkey'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(PROJECT_ROOT, 'test.sqlite')
+        'NAME': os.path.join(DATA_ROOT, 'test.sqlite')
     }
 }
 
-MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media')
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+MEDIA_ROOT = os.path.join(DATA_ROOT, 'media')
+STATIC_ROOT = os.path.join(DATA_ROOT, 'static')
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_FROM_ADDRESS = 'projekt.xyz@riseup.net'
