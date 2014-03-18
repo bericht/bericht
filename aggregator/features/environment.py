d0674ddf7263596d572fc57f76eea7308c2a0fe7
sypa <syp@riseup.net>
2014-03-18 07:06:45 +0200

    A new attempt at sauce_connect...
    ...modelled after https://github.com/santiycr/cssify

--- aggregator/features/environment.py
+++ aggregator/features/environment.py
@@ -14,13 +14,21 @@ def before_all(context):
     access_key = os.environ.get('SAUCE_ACCESS_KEY')
 
     if username and access_key:
-        server_url = 'http://%s:%s@localhost:4445/wd/hub' % (username,
-                                                             access_key)
+        print("Launching sauce_connect...")
+        hub_url = '%s:%s@ondemand.saucelabs.com:80' % (username, access_key)
         capabilities = {
-            'tunnel_identifier': os.environ['TRAVIS_JOB_NUMBER'],
+            'tunnel-identifier': os.environ['TRAVIS_JOB_NUMBER'],
+            'build': os.environ['TRAVIS_BUILD_NUMBER'],
+            'tags': [os.environ['TRAVIS_PYTHON_VERSION'], 'CI'],
             'platform': 'Linux',
         }
-        context.browser = Browser('remote', server_url, **capabilities)
+        context.browser = Browser(
+            'remote',
+            desired_capabilities=capabilities,
+            command_executor="http://%s/wd/hub" % hub_url)
+        print("Sauce Labs job: https://saucelabs.com/jobs/%s" %
+              context.browser.session_id)
+        context.browser.implicitly_wait(30)
     else:
         context.browser = Browser(browser)
 
