352851d10d5bea94416ab8a537ec6068f0d062ba
LexieDee <lexie.dvorak@gmail.com>
2014-03-25 00:09:12 +0100

    Moved Article._cleanup(..) and Article.contains_text(..) to artex (as module-level public methods) and added test for contains_text(..)

--- artex/artex.py
+++ artex/artex.py
@@ -2,6 +2,42 @@ from readability import Document
 from lxml import etree
 
 
+def contains_text(element):
+    """
+    Checks if the element contains any text before, after or between child
+    elements.
+    """
+    if element.text is not None and element.text.strip() != '':
+        return True
+    for child in list(element):
+        if child.tail is not None and child.tail.strip() != '':
+            return True
+    return False
+
+
+def cleanup(root, title):
+    """
+    Remove redundant outmost divs until one remains as root. Also check
+    if the div contains text that is not wrapped in other elements so
+    that we don't loose the content. Also remove headers at the start if
+    they contain the title of the article.
+    """
+    # remove outer redundant divs
+    while root.tag == 'div' and len(list(root)) == 1:
+        # make sure there is no non-whitespace content beyond children
+        if not contains_text(root):
+            root = root[0]
+        else:
+            break
+    # if the first child is a header and its text equals the title
+    # it is redundant and gets removed.
+    if root[0].tag in ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'] and \
+       root[0].text == title:
+        root = root[1:]
+    return u''.join(map(lambda a: etree.tostring(a).strip(),
+                        list(root)))
+
+
 class Article():
     """
     Wraps around readability.Document and provides access to the article
@@ -26,38 +62,5 @@ class Article():
         # obtain the article as HtmlElement tree:
         html_tree = doc.html
         # clean up the article html:
-        self.content = self._cleanup(html_tree, self.title)
+        self.content = cleanup(html_tree, self.title)
 
-    def _cleanup(self, root, title):
-        """
-        Remove redundant outmost divs until one remains as root. Also check
-        if the div contains text that is not wrapped in other elements so
-        that we don't loose the content. Also remove headers at the start if
-        they contain the title of the article.
-        """
-        # remove outer redundant divs
-        while root.tag == 'div' and len(list(root)) == 1:
-            # make sure there is no non-whitespace content beyond children
-            if not self._contains_text(root):
-                root = root[0]
-            else:
-                break
-        # if the first child is a header and its text equals the title
-        # it is redundant and gets removed.
-        if root[0].tag in ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'] and \
-           root[0].text == title:
-            root = root[1:]
-        return u''.join(map(lambda a: etree.tostring(a).strip(),
-                            list(root)))
-
-    def _contains_text(self, element):
-        """
-        Checks if the element contains any text before, after or between child
-        elements.
-        """
-        if element.text is not None and element.text.strip() != '':
-            return True
-        for child in list(element):
-            if child.tail is not None and child.tail.strip() != '':
-                return True
-        return False
--- artex/tests.py
+++ artex/tests.py
@@ -8,8 +8,9 @@ import unittest
 
 from django.test import TestCase
 from django.conf import settings
-
+from lxml import etree
 from artex import Article
+import artex
 
 
 class ArticleTest(TestCase):
@@ -19,6 +20,7 @@ class ArticleTest(TestCase):
                                          'artex/fixtures/')
 
     def test_Artex_djangoblog(self):
+        """Tests correct article extraction on a Djangoproject Blog post."""
         html_file = os.path.join(self.fixtures_dir, 'django-blogpost.html')
         html = open(html_file, 'r').read()
         article = Article(html)
@@ -27,9 +29,28 @@ class ArticleTest(TestCase):
         self.assertEqual(article.content[-10:], ', 2014</p>')
 
     def test_Artex_wordpress(self):
+        """Tests correct article extraction on a WordPress.org blog post."""
         html_file = os.path.join(self.fixtures_dir, 'wordpress-blogpost.html')
         html = open(html_file, 'r').read()
         article = Article(html)
         self.assertEqual(article.title, u'WordPress 3.8 “Parker”')
         self.assertEqual(article.content[:20], '<p>Version 3.8 of Wo')
         self.assertEqual(article.content[-20:], 'for version 3.9!</p>')
+
+    def test_contains_text_before_p(self):
+        """Tests that text in the outer div before a p is retained."""
+        html = '<div>this is valuable text<p>and here is more</p></div>'
+        self.assertTrue(artex.contains_text(etree.fromstring(html)))
+
+    def test_contains_text_between_p(self):
+        """Tests that text in the outer div before a p is retained."""
+        html = '''
+        <div><p>This is text. </p>This is valuable text<p>and
+        here is more</p></div>
+        '''
+        self.assertTrue(artex.contains_text(etree.fromstring(html)))
+
+    def test_contains_text_after_p(self):
+        """Tests that text in the outer div before a p is retained."""
+        html = '<div><p>This is text. </p>This is valuable text.</div>'
+        self.assertTrue(artex.contains_text(etree.fromstring(html)))
