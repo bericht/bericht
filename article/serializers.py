3a932aeb90318fcea9456ad81cdbdea0e0446690
sypa <syp@riseup.net>
2014-04-09 06:31:44 +0300

    re-add the source field to article list view.

--- article/serializers.py
+++ article/serializers.py
@@ -1,14 +1,21 @@
 from rest_framework import serializers
-from .models import Article
+from .models import Article, ImportedArticle
 
 
 class ArticleSerializer(serializers.ModelSerializer):
     slug = serializers.CharField(source='slug', read_only=True)
-    source = serializers.CharField(source='feeditem.feed.title',
-                                   read_only=True)
+    source = serializers.SerializerMethodField('get_source_info')
     tags = serializers.CharField(source='tags.names', read_only=True)
 
     class Meta:
         model = Article
         fields = ('id', 'slug', 'title', 'tags', 'content', 'updated_at',
-                  )  # 'source')
+                  'source')
+
+    def get_source_info(self, obj):
+        article = obj.get_child_class_instance()
+        if isinstance(article, ImportedArticle):
+            return {'title': article.feeditem.feed.title,
+                    'url': article.feeditem.link}
+        else:
+            return None
