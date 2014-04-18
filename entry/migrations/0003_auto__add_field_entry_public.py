eb1f0c6d34263ec4adc6dfd2d6cd97ad07819540
sypa <syp@riseup.net>
2014-04-18 18:20:12 +0300

    cache the results of Entry.is_public() in Entry.public.

---
+++ entry/migrations/0003_auto__add_field_entry_public.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'Entry.public'
+        db.add_column(u'entry_entry', 'public',
+                      self.gf('django.db.models.fields.BooleanField')(default=False),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Deleting field 'Entry.public'
+        db.delete_column(u'entry_entry', 'public')
+
+
+    models = {
+        u'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        u'entry.entry': {
+            'Meta': {'object_name': 'Entry'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'polymorphic_ctype': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "u'polymorphic_entry.entry_set'", 'null': 'True', 'to': u"orm['contenttypes.ContentType']"}),
+            'public': ('django.db.models.fields.BooleanField', [], {'default': 'False'})
+        }
+    }
+
+    complete_apps = ['entry']
--- entry/models.py
+++ entry/models.py
@@ -1,11 +1,23 @@
+from django.db import models
 from polymorphic import PolymorphicModel
 from ..voting.models import Vote
 
 
 class Entry(PolymorphicModel):
+    # cache the result of is_public here to make it easier to query.
+    public = models.BooleanField(default=False)
+
     def is_public(self):
+        """
+        Change this method here or in a child class to (selectivly) override
+        the criteria for public Entries.
+        """
         votes = Vote.objects.get_votes(self)
         if votes['NO'] < 10 and votes['VETO'] < 1 and votes['YES'] >= 10:
             return True
         else:
             return False
+
+    def save(self, *args, **kwargs):
+        self.public = self.is_public()
+        super(Entry, self).save(*args, **kwargs)
