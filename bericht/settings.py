5718a1f57df49feafc8d3e9c319cacec25f5d27a
sypa <sypa@internet>
2014-05-03 00:36:01 +0300

    fix PROJECT_ROOT

--- bericht/settings.py
+++ bericht/settings.py
@@ -5,7 +5,7 @@
 import os
 
 # Full filesystem path to the project.
-PROJECT_ROOT = os.path.join(os.path.dirname(os.path.abspath(__file__)), "bericht")
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Name of the directory for the project.
 PROJECT_DIRNAME = PROJECT_ROOT.split(os.sep)[-1]
