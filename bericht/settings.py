7caec43fe669194ad05a0cbdaf51f71efa574d83
sypa <sypa@internet>
2014-05-03 12:11:13 +0300

    path adjustments in settings and manage.py.

--- bericht/settings.py
+++ bericht/settings.py
@@ -41,7 +41,7 @@ ROOT_URLCONF = "%s.urls" % PROJECT_DIRNAME
 # or "C:/www/django/templates".
 # Always use forward slashes, even on Windows.
 # Don't forget to use absolute paths, not relative paths.
-TEMPLATE_DIRS = (os.path.join(PROJECT_ROOT, "/theme/templates"),)
+TEMPLATE_DIRS = (os.path.join(PROJECT_ROOT, "theme/templates"),)
 JSTEMPLATE_DIRS = [os.path.join(d, 'mustache') for d in TEMPLATE_DIRS]
 
 ####################
